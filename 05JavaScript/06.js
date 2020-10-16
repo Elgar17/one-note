2f36c64fbac616b5cce97fdc31541d22aa589c1d
Elgar17 <elgara@163.com>
2020-10-16 22:51:39 +0800

    更新na

--- 05JavaScript/06.js
+++ 05JavaScript/06.js
@@ -2,28 +2,55 @@
 // 2. 判断数组还是对象
 // 3. 建立对应的数组或对象
 
-function deepClone(origin, target) {
+
+// 第一种方法要判断数组和对象
+ function deepClone1(origin, target) {
     var target = target || {},
-        toStr = Object.prototype.toString,
+        isArr = Object.prototype.toString,
         arrStr = "[object Array]";
 
-    for (var prop in origin) {
-        if (origin.hasOwnProperty(prop)) {
-            if (tyeof[prop] !== "null" &&  typeof (origin[prop] == 'object')) {
-                // 判断数组
-                // if (toStr.call(origin[prop] == arrStr)) {
-                //     target[prop] = []
-                // } else {
-                //     target[prop] = {}
-                // }
-                target[prop] = (toStr.call(origin[prop] == arrStr))
-                ? [] : {}
-
-                deepClone(origin[prop], target[prpo])
+    for (var pr in origin) {
+        if (origin.hasOwnProperty(pr)) {
+            if (origin[pr] !== null && typeof (origin[pr]) == "object") {
+                target[pr] = (isArr.call(origin[pr]) == arrStr) ? [] : {};
+                deepClone1(origin[pr], target[pr])
             } else {
-                target[prop] = origin[prop]
+                target[pr] = origin[pr]
             }
         }
     }
-    return target;
+    return target
 }
+
+// 不用判断数组对象
+function deepClone(obj) {
+	if (obj === null) return obj;
+    // if (obj instanceof Date) return new Data(obj);
+	// if (obj instanceof RegExp) return new RegExp(obj);
+    if (typeof obj !== "object") return obj;
+	var cloneObj = new obj.constructor();
+    for (var i in obj) {
+    	if (obj.hasOwnProperty(i)) {
+           cloneObj[i] = deepClone(obj[i]);	// 递归
+        }
+    }
+    return cloneObj;
+}
+
+
+
+var obj = {
+    name: "car",
+    arr: [1, 3, 4, 6],
+    color: {
+        one: "red"
+    },
+    say: function(){
+        console.log(this)
+    }
+}
+
+console.log("lod", obj) // new { name: 'car', arr: [ 1, 3, 4, 6 ], color: { one: 'red' } }
+var newObj = deep(obj)
+console.log("new", newObj) // new { name: 'car', arr: [ 1, 3, 4, 6 ], color: { one: 'red' } }
+
